67b5283e7d64b6200ecaa26f626cd9d433251645
Desmond Chi <dcchi@live.com>
2022-08-27 14:24:27 -0500

    Added for loop that prints out 1-5.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main() {
+    // Print out 1=5.
+    for (int i = 0; i < 5; ++i) {
+        std::cout << i + 1<< std::endl;
+    }
+
     // This is a comment.
     std::cout << "Hello World" << std::endl;
 }
